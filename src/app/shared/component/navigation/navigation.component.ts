e0674ec22cac29816a3fb71b54916dce9370d10a
aleDevs <lacroce.devs@gmail.com>
2022-07-18 00:24:29 +0200

    init dark mode themes

--- src/app/shared/component/navigation/navigation.component.ts
+++ src/app/shared/component/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import {AuthService} from "../../services/auth.service";
 
 @Component({
@@ -7,12 +8,25 @@ import {AuthService} from "../../services/auth.service";
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
+  toggleTheme = new FormControl();
 
   constructor(
-    public authService: AuthService
+    public authService: AuthService,
+    private _renderer: Renderer2
   ) { }
 
   ngOnInit(): void {
+    this.toggleTheme.valueChanges.subscribe(toggleValue => {
+      console.log(toggleValue);
+      
+      if (toggleValue === true) {
+        this._renderer.addClass(document.body, 'dark-theme');
+        this._renderer.removeClass(document.body, 'light-theme');
+      } else {
+        this._renderer.addClass(document.body, 'light-theme');
+        this._renderer.removeClass(document.body, 'dark-theme');
+      }
+    });
   }
 
 }
